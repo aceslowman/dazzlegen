94d8f4391afd6a93cae4e8624098dbd90f37b72f
Glitch (hello-express) <none>
2021-04-10 17:17:48 +0000

    🕵😅 Checkpoint ./src/App.js:24872531/6159 ./src/index.js:24872531/268 ./src/index.css:24872531/366 ./src/components/sketch.js:24872531/22707 ./src/components/InputFloat.js:24872531/3200 ./src/components/InputGroup.js:24872531/1379 ./src/components/SketchContainer.js:24872531/16639 ./package.json:24872531/10246 ./webpack.config.js:24872531/992

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HTMLwebpackplugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLwebpackplugin({
+      template: "./public/index.html"
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    publicPath: "/",
+    overlay: true,
+    compress: true,
+    hot: false,
+    inline: false,
+    liveReload: false,
+    disableHostCheck: true,
+    port: process.env.PORT,
+    public: "camogen-lite.glitch.me",
+    // https: true,
+    // proxy: {
+    //   "/api": "http://localhost:3001"
+    // }
+  }
+};
