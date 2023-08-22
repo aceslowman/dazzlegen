23efa4e0ac80eb3a96c265a921e6299e4c0cae05
Austin Slominski <austin.slominski@gmail.com>
2023-08-22 09:20:00 -0600

    mostly functional again!

--- Dazzle.js
+++ Dazzle.js
@@ -1,6 +1,7 @@
 /* 
-    - [] layers are now layers
-    - [] glyphs should be a member of layers
+    - [x] layers are now layers
+    - [x] glyphs should be a member of layers
+    - [x] get rid of method chaining?
  */
 
 import { stringGen } from "./js/Utilities.js";
@@ -8,86 +9,143 @@ import Glyph from "./js/Glyph.js";
 
 let glyphs = [];
 let layers;
-let generateFlag = false;
-let snapshotFlag = false;
 
-let cnv;
+let bAutoGenerate = false;
 
-window.workAreaElement = document.querySelector("#workArea");
-window.workAreaBounds = workAreaElement.getBoundingClientRect();
+let cnv, pg;
 
 /* describes the behavior of each successive layer */
 layers = [
   {
     glyphs: [],
-    dim: [30, 30],
-    seed: Math.random() * 1000,
+    dim: { x: 30, y: 30 },
+    seed: Math.floor(Math.random() * 1000),
     noise: {
       scale: 0.1,
       steps: 8,
     },
+    margin: { x: 0, y: 0 },
+    padding: { x: 0, y: 0 },
   },
   {
     glyphs: [],
-    dim: [3, 3],
-    seed: Math.random() * 1000,
+    dim: { x: 3, y: 3 },
+    seed: Math.floor(Math.random() * 1000),
     noise: {
       scale: 0.1,
       steps: 6,
     },
+    margin: { x: 0, y: 0 },
+    padding: { x: 0, y: 0 },
   },
 ];
 
 window.setup = () => {
-  cnv = createCanvas(window.workAreaBounds.width, window.workAreaBounds.width);
+  window.workAreaElement = document.querySelector("#workArea");
+  window.workAreaBounds = workAreaElement.getBoundingClientRect();
+  let s =
+    window.workAreaBounds.width > window.workAreaBounds.height
+      ? window.workAreaBounds.height
+      : window.workAreaBounds.width;
+  resizeCanvas(s - 30, s - 30);
+  cnv = createCanvas(s - 30, s - 30);
   cnv.parent("workArea");
 
-  smooth();
+  /* graphics are drawn to pg and are resampled for the main canvas */
+  pg = createGraphics(window.workAreaBounds.width, window.workAreaBounds.width);
+  let ctx = cnv.canvas.getContext("2d");
+  ctx.mozImageSmoothingEnabled = false;
+  ctx.webkitImageSmoothingEnabled = false;
+  ctx.msImageSmoothingEnabled = false;
+  ctx.imageSmoothingEnabled = false;
+
   colorMode(HSB, 255);
   background(255);
 
   generate();
+
+  setupInterface();
+};
+
+window.windowResized = () => {
+  window.workAreaElement = document.querySelector("#workArea");
+  window.workAreaBounds = workAreaElement.getBoundingClientRect();
+  let s =
+    window.workAreaBounds.width > window.workAreaBounds.height
+      ? window.workAreaBounds.height
+      : window.workAreaBounds.width;
+  resizeCanvas(s - 30, s - 30);
+  image(pg, 0, 0, width, height);
 };
 
 let generate = () => {
   background(255);
+  pg.background(255);
 
   glyphs = [];
 
   glyphs.push(
-    new Glyph()
-      .anchor(0, 0)
-      .dim(layers[0].dim[0], layers[0].dim[1])
-      .size(width, height)
-      .seed(layers[0].seed)
-      .noise(layers[0].noise.scale, layers[0].noise.steps)
-      // .stroke(0)
-      .fill(layers[0].seed * 255)
-    // .padding(10,10)
-    // .draw()
+    new Glyph({
+      anchor: {
+        x: 0,
+        y: 0,
+      },
+      dim: {
+        ...layers[0].dim,
+      },
+      size: {
+        width: pg.width,
+        height: pg.height,
+      },
+      seed: layers[0].seed,
+      noise: {
+        scale: layers[0].noise.scale,
+        steps: layers[0].noise.steps, // REVISIT
+      },
+      stroke_color: undefined,
+      fill_color: layers[0].seed * 255,
+      padding: { x: 0, y: 0 },
+      margin: { x: 0, y: 0 },
+    })
   );
 
+  glyphs[0].noise();
+  glyphs[0].draw(pg);
   glyphs[0].next((t, x, y, i) => next_func(t, x, y, i, 1));
 
-  // console.log({glyphs})
+  image(pg, 0, 0, width, height);
 };
 
 let next_func = (t, x, y, i, l) => {
-  let w = t.width / t.x_dim;
-  let h = t.height / t.y_dim;
+  let w = t.size.width / t.dim.x;
+  let h = t.size.height / t.dim.y;
 
-  let glyph = new Glyph()
-    .anchor(t.x_anchor + x * w + t.x_padding, t.y_anchor + y * h + t.y_padding)
-    .dim(layers[l].dim[0], layers[l].dim[1])
-    .size(w - t.x_padding, h - t.y_padding)
-    .seed(layers[l].seed + t.cells[i])
-    .noise(layers[l].noise.scale, layers[1].noise.steps)
-    // .stroke(255)
-    // .fill(t.cells[i]*255)
-    .fill(0)
-    // .fill(0)
-    // .padding(1,1)
-    .draw();
+  let glyph = new Glyph({
+    anchor: {
+      x: t.anchor.x + x * w + t.padding.x,
+      y: t.anchor.y + y * h + t.padding.y,
+    },
+    dim: {
+      x: layers[l].dim.x,
+      y: layers[l].dim.y,
+    },
+    size: {
+      width: w - t.padding.x,
+      height: h - t.padding.y,
+    },
+    seed: layers[l].seed + t.cells[i],
+    noise: {
+      scale: layers[l].noise.scale,
+      steps: layers[l].noise.steps, // REVISIT
+    },
+    stroke_color: undefined,
+    fill_color: 0,
+    padding: { x: 0, y: 0 },
+    margin: { x: 0, y: 0 },
+  });
+
+  glyph.noise();
+  glyph.draw(pg);
 
   // (previous glyph, x coord, y coord, cell index)
   glyphs.push(glyph);
@@ -96,4 +154,195 @@ let next_func = (t, x, y, i, l) => {
     l += 1;
     glyph.next((t, x, y, i) => next_func(t, x, y, i, l));
   }
+};
+
+const randomizeAll = () => {
+  for (let i = 0; i < layers.length; i++) {
+    let layer = layers[i];
+    let layerControl = document.querySelector("#layer_" + i);
+
+    layers[i] = {
+      ...layer,
+      glyphs: [],
+      dim: {
+        x: Math.floor(Math.random() * 50),
+        y: Math.floor(Math.random() * 50),
+      },
+      seed: Math.floor(Math.random() * 1000),
+      noise: {
+        scale: Math.random() * 5,
+        steps: Math.random() * 5,
+      },
+      margin: { x: Math.random(), y: Math.random() },
+      padding: { x: Math.random(), y: Math.random() },
+    };
+
+    layerControl.querySelector(".dimX input").value = layer.dim.x;
+    layerControl.querySelector(".dimY input").value = layer.dim.y;
+    layerControl.querySelector(".marginX input").value = layer.margin.x;
+    layerControl.querySelector(".marginY input").value = layer.margin.y;
+    layerControl.querySelector(".paddingX input").value = layer.padding.x;
+    layerControl.querySelector(".paddingY input").value = layer.padding.y;
+    layerControl.querySelector(".seed input").value = layer.seed;
+    layerControl.querySelector(".noiseScale input").value = layer.noise.scale;
+    layerControl.querySelector(".noiseSteps input").value = layer.noise.steps;
+  }
+
+  if (bAutoGenerate) generate();
+};
+
+const setupInterface = () => {
+  document.querySelector("#layersControl").textContent = "";
+
+  /* add level */
+  document.querySelector("#addLevelButton").addEventListener("click", (e) => {
+    layers.push({
+      glyphs: [],
+      dim: { x: 3, y: 3 },
+      seed: Math.floor(Math.random() * 1000),
+      noise: {
+        scale: 0.1,
+        steps: 6,
+      },
+      margin: { x: 0, y: 0 },
+      padding: { x: 0, y: 0 },
+    });
+    setupInterface();
+  });
+
+  /* remove level */
+  document
+    .querySelector("#removeLevelButton")
+    .addEventListener("click", (e) => {
+      layers.pop();
+      setupInterface();
+    });
+
+  /* snapshot */
+  document.querySelector("#snapshotButton").addEventListener("click", (e) => {
+    snapshot();
+  });
+
+  /* dimension select */
+  document.querySelector("#resolutionSelect").addEventListener("input", (e) => {
+    pg = createGraphics(Number(e.target.value), Number(e.target.value));
+    if (bAutoGenerate) generate();
+  });
+  /* TODO load from local storage */
+  document.querySelector("#resolutionSelect").value = 512;
+
+  /* .randomize all */
+  document
+    .querySelector("#randomizeAllButton")
+    .addEventListener("click", (e) => {
+      randomizeAll();
+    });
+
+  /* .generate */
+  document.querySelector("#generateButton").addEventListener("click", (e) => {
+    generate();
+  });
+
+  /* auto generate */
+  document.querySelector("#autoGenTick").checked = bAutoGenerate;
+  document.querySelector("#autoGenTick").addEventListener("input", (e) => {
+    bAutoGenerate = Boolean(e.target.checked);
+  });
+
+  for (let i = 0; i < layers.length; i++) {
+    let layer = layers[i];
+    let layerControl = document
+      .querySelector("#layerControlTemplate")
+      .cloneNode(true);
+    layerControl.id = "layer_" + i;
+
+    layerControl.querySelector(".layerLabel").innerText = "layer_" + i;
+
+    /* dim */
+    layerControl.querySelector(".dimX input").value = layer.dim.x;
+    layerControl.querySelector(".dimX input").addEventListener("input", (e) => {
+      let v = Number(e.target.value);
+      layer.dim.x = v;
+      if (bAutoGenerate) generate();
+    });
+
+    layerControl.querySelector(".dimY input").value = layer.dim.y;
+    layerControl.querySelector(".dimY input").addEventListener("input", (e) => {
+      let v = Number(e.target.value);
+      layer.dim.y = v;
+      if (bAutoGenerate) generate();
+    });
+
+    /* margin */
+    layerControl.querySelector(".marginX input").value = layer.margin.x;
+    layerControl
+      .querySelector(".marginX input")
+      .addEventListener("input", (e) => {
+        let v = Number(e.target.value);
+        layer.margin.x = v;
+        if (bAutoGenerate) generate();
+      });
+
+    layerControl.querySelector(".marginY input").value = layer.margin.y;
+    layerControl
+      .querySelector(".marginY input")
+      .addEventListener("input", (e) => {
+        let v = Number(e.target.value);
+        layer.margin.y = v;
+        if (bAutoGenerate) generate();
+      });
+
+    /* padding */
+    layerControl.querySelector(".paddingX input").value = layer.padding.x;
+    layerControl
+      .querySelector(".paddingX input")
+      .addEventListener("input", (e) => {
+        let v = Number(e.target.value);
+        layer.padding.x = v;
+        if (bAutoGenerate) generate();
+      });
+
+    layerControl.querySelector(".paddingY input").value = layer.padding.y;
+    layerControl
+      .querySelector(".paddingY input")
+      .addEventListener("input", (e) => {
+        let v = Number(e.target.value);
+        layer.padding.y = v;
+        if (bAutoGenerate) generate();
+      });
+
+    /* seed */
+    layerControl.querySelector(".seed input").value = layer.seed;
+    layerControl.querySelector(".seed input").addEventListener("input", (e) => {
+      let v = Number(e.target.value);
+      layer.seed = v;
+      if (bAutoGenerate) generate();
+    });
+
+    /* noise scale */
+    layerControl.querySelector(".noiseScale input").value = layer.noise.scale;
+    layerControl
+      .querySelector(".noiseScale input")
+      .addEventListener("input", (e) => {
+        let v = Number(e.target.value);
+        layer.noise.scale = v;
+        if (bAutoGenerate) generate();
+      });
+
+    /* noise steps */
+    layerControl.querySelector(".noiseSteps input").value = layer.noise.steps;
+    layerControl
+      .querySelector(".noiseSteps input")
+      .addEventListener("input", (e) => {
+        let v = Number(e.target.value);
+        layer.noise.steps = v;
+        if (bAutoGenerate) generate();
+      });
+
+    document.querySelector("#layersControl").appendChild(layerControl);
+  }
+};
+
+const snapshot = () => {
+  saveCanvas(pg, "dazzlegen_" + stringGen(6));
 };
--- js/Glyph.js
+++ js/Glyph.js
@@ -1,126 +1,79 @@
 export default class Glyph {
-    constructor() {
-      this.cells = [];
-  
-      this._seed = random(1000);
-  
-      this.width = 100;
-      this.height = 100;
-  
-      this.x_dim = 4;
-      this.y_dim = 4;
-  
-      this.x_anchor = 0;
-      this.y_anchor = 0;
-  
-      this.x_padding = 0;
-      this.y_padding = 0;
-  
-      this.x_margin = 5;
-      this.y_margin = 5;
-    }
-  
-    anchor(x, y) {
-      this.x_anchor = x;
-      this.y_anchor = y;
-      return this;
-    }
-  
-    padding(x, y) {
-      this.x_padding = x;
-      this.y_padding = y;
-      return this;
-    }
-  
-    dim(x, y) {
-      this.x_dim = x;
-      this.y_dim = y;
-      return this;
-    }
-  
-    size(x, y) {
-      this.width = x;
-      this.height = y;
-      return this;
-    }
-  
-    seed(s) {
-      this._seed = s;
-      return this;
-    }
-  
-    noise(scale, steps) {
-      /* set seed for random generation */
-      noiseSeed(this._seed);
-  
-      for (let _x = 0; _x < this.x_dim; _x++) {
-        for (let _y = 0; _y < this.y_dim; _y++) {
-          /* set the color of the cell according to its position and scale */
-          let cell = noise(_x * scale + this._seed, _y * scale + this._seed);
-  
-          if (steps !== undefined) {
-            cell = floor(cell * (steps + 1)) / (steps - 1);
-          }
-  
-          this.cells.push(cell);
+  constructor(options) {
+    this.cells = [];
+    this.seed = options.seed;
+    this.size = options.size;
+    this.dim = options.dim;
+    this.anchor = options.anchor;
+    this.padding = options.padding;
+    this.margin = options.margin;
+    this.stroke_color = options.stroke_color;
+    this.fill_color = options.fill_color;
+    this.noiseParams = options.noise;
+  }
+
+  noise() {
+    /* set seed for random generation */
+    noiseSeed(this.seed);
+
+    for (let _x = 0; _x < this.dim.x; _x++) {
+      for (let _y = 0; _y < this.dim.y; _y++) {
+        /* set the color of the cell according to its position and scale */
+        let cell = noise(
+          _x * this.noiseParams.scale + this.seed,
+          _y * this.noiseParams.scale + this.seed
+        );
+
+        if (this.noiseParams.steps !== undefined) {
+          cell =
+            floor(cell * (this.noiseParams.steps + 1)) /
+            (this.noiseParams.steps - 1);
         }
+
+        this.cells.push(cell);
       }
-  
-      return this;
-    }
-  
-    stroke(c) {
-      this.stroke_color = c;
-      return this;
-    }
-  
-    fill(c) {
-      this.fill_color = c;
-      return this;
-    }
-  
-    next(f) {
-      for (let _x = 0, i = 0; _x < this.x_dim; _x++) {
-        for (let _y = 0; _y < this.y_dim; _y++, i++) {
-          f(this, _x, _y, i); //move to draw
-        }
-      }
-    }
-  
-    draw() {
-      /*  
-          this really just draws each cell to it's position, 
-          does not generate noise itself
-        */
-      for (let _x = 0, i = 0; _x < this.x_dim; _x++) {
-        for (let _y = 0; _y < this.y_dim; _y++, i++) {
-          let pos_x = (_x / this.x_dim) * this.width;
-          pos_x += this.x_anchor;
-  
-          let pos_y = (_y / this.y_dim) * this.height;
-          pos_y += this.y_anchor;
-  
-          this.stroke_color !== undefined
-            ? stroke(this.stroke_color)
-            : noStroke();
-          this.fill_color !== undefined
-            ? fill(this.fill_color, this.cells[i] > 0.5 ? 255 : 0)
-            : noFill();
-  
-          rect(
-            floor(pos_x),
-            floor(pos_y),
-            ceil(this.width / this.x_dim),
-            ceil(this.height / this.y_dim)
-          );
-  
-          // debug numbers
-          // fill(128)
-          // text(this.cells[i].toFixed(2),pos_x+(this.height / this.x_dim)/2,pos_y+(this.height / this.y_dim)/2);
-        }
-      }
-  
-      return this;
     }
   }
-  
+
+  next(f) {
+    for (let _x = 0, i = 0; _x < this.dim.x; _x++) {
+      for (let _y = 0; _y < this.dim.y; _y++, i++) {
+        f(this, _x, _y, i); //move to draw
+      }
+    }
+  }
+
+  draw(pg) {
+    /*  
+        this really just draws each cell to it's position, 
+        does not generate noise itself
+    */
+    for (let _x = 0, i = 0; _x < this.dim.x; _x++) {
+      for (let _y = 0; _y < this.dim.y; _y++, i++) {
+        let pos_x = (_x / this.dim.x) * this.size.width;
+        pos_x += this.anchor.x;
+
+        let pos_y = (_y / this.dim.y) * this.size.height;
+        pos_y += this.anchor.y;
+
+        this.stroke_color !== undefined
+          ? pg.stroke(this.stroke_color)
+          : pg.noStroke();
+        this.fill_color !== undefined
+          ? pg.fill(this.fill_color, this.cells[i] > 0.5 ? 255 : 0)
+          : pg.noFill();
+
+        pg.rect(
+          floor(pos_x),
+          floor(pos_y),
+          ceil(this.size.width / this.dim.x),
+          ceil(this.size.height / this.dim.y)
+        );
+
+        // debug numbers
+        // fill(128)
+        // text(this.cells[i].toFixed(2),pos_x+(this.height / this.x_dim)/2,pos_y+(this.height / this.y_dim)/2);
+      }
+    }
+  }
+}
